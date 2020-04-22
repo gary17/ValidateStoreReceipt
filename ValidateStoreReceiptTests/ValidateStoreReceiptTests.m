e26d38d824151c69dd2e82591f748b665c201813
Gabriel <16545056+gary17@users.noreply.github.com>
2020-04-22 13:43:33 +0200

    Add a unit test with an attempt to corrupt a valid receipt, in order to confirm detection of a validation failure

--- ValidateStoreReceiptTests/ValidateStoreReceiptTests.m
+++ ValidateStoreReceiptTests/ValidateStoreReceiptTests.m
@@ -70,4 +70,97 @@
 					withMachineIdentifier:machineIdentifier]); // mismatch
 }
 
+- (void)testValidateReceipt005 {
+	NSURL *receiptURL = [[NSBundle mainBundle] URLForResource:@"AppleAppStoreSampleReceipt" withExtension:@"bin"];
+	NSString *pathToReceiptLHS = [receiptURL path];
+
+	NSString *tmpDir = NSTemporaryDirectory();
+	NSString *pathToReceiptRHS = [tmpDir stringByAppendingPathComponent:@"AppleAppStoreSampleReceipt2.bin"];
+
+	//
+	
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+
+	// perform a number of attempts to corrupt a receipt to confirm detection of a validation failure
+	const NSUInteger CORRUPTION_ATTEMPT_COUNT = 100;
+
+	for (NSUInteger i = 0; i < CORRUPTION_ATTEMPT_COUNT; i++)
+	{
+		NSError *error = nil;
+
+		// clean up
+
+		if ([fileManager fileExistsAtPath:pathToReceiptRHS])
+		{
+			[fileManager removeItemAtPath:pathToReceiptRHS error:&error];
+			XCTAssert(error == nil);
+		}
+		
+		XCTAssert([fileManager fileExistsAtPath:pathToReceiptLHS]);
+		XCTAssertFalse([fileManager fileExistsAtPath:pathToReceiptRHS]);
+		
+		// make a copy of the original receipt
+		
+		[fileManager copyItemAtPath:pathToReceiptLHS toPath:pathToReceiptRHS error:&error];
+		
+		XCTAssert(error == nil);
+		XCTAssert([fileManager fileExistsAtPath:pathToReceiptRHS]);
+		
+		// sanity: successfully validate the copy of the original receipt
+		
+		XCTAssert([ValidateStoreReceipt validateReceiptAtPath:pathToReceiptRHS
+				withBundleIdentifier:nil // disregard
+					withBundleVersion:nil // disregard
+						withMachineIdentifier:nil]); // disregard
+						
+		// change (thus corrupt) a number of bytes at random within the copy of the original receipt
+		
+		// FIXME: corrupting fewer than 5 bytes at a time sometimes goes undetected - find out why that is, exactly
+		const NSUInteger CORRUPTED_BYTE_COUNT = 5;
+		
+		NSData *data = [NSData dataWithContentsOfFile:pathToReceiptRHS options:NSDataReadingUncached error:&error];
+
+		XCTAssert(error == nil);
+		XCTAssert(data.length > 0);
+		
+		NSMutableData *mutable = [NSMutableData dataWithData:data]; // only ~5Kb, copy in its entirety
+		
+		for (NSUInteger v = 0; v < CORRUPTED_BYTE_COUNT; v++)
+		{
+			// FIXME: we might process the same byte (at the same random index) more than once,
+			// thus corrupting it twice or even by chance reverting it back to the original, but
+			// such coincidence is probably statistically insignificant
+
+			const NSUInteger randomIndex = arc4random_uniform((uint32_t) data.length - 1); // from zero, inclusive
+			
+			for (;;) {
+				const Byte randomByte[] = { arc4random_uniform(255) };
+				const NSRange range = NSMakeRange(randomIndex, sizeof(randomByte));
+				
+				// get the valid (unchanged) byte
+				
+				Byte originalByte[] = { 0 };
+				[mutable getBytes:originalByte range:range];
+				
+				// make sure it is, indeed, changed to something different, thus an invalid byte
+
+				if (randomByte[0] != originalByte[0])
+				{
+					[mutable replaceBytesInRange:range withBytes:randomByte];
+					break;
+				}
+			}
+			
+			XCTAssert([mutable writeToFile:pathToReceiptRHS atomically:YES]);
+		}
+
+		// re-validate the copy of the original receipt
+		
+		XCTAssertFalse([ValidateStoreReceipt validateReceiptAtPath:pathToReceiptRHS
+				withBundleIdentifier:nil // disregard
+					withBundleVersion:nil // disregard
+						withMachineIdentifier:nil]); // disregard
+	} // for
+}
+
 @end
